973d7286d78e240522344755674c7944036a8c50
Weydson Fernandes <weydfernandes@gmail.com>
2025-01-07 15:03:08 -0300

    inserindo dados de cartão e chave pix

--- INSERT_MOVIMENTACAO.sql
+++ INSERT_MOVIMENTACAO.sql
@@ -11,7 +11,7 @@ select * from TB_USUARIO
 
 go
 insert into TB_CONTA VALUES 
-(3, '2123', '27307548-2', 0, '1234', 1);
+(1, '2123', '27307548-2', 0, '1234', 1);
 
 select * from TB_CONTA
 
@@ -23,10 +23,27 @@ select * from TB_STATUS_MOVIMENTACAO
 
 go
 INSERT into TB_MOVIMENTACAO VALUES 
-(3, 2, 'Pagamento de boleto','C', getdate(), 'pagamento de boleto', 100, 0, 100, NEWID());
+	(3, 1, 'Pagamento de boleto','C', getdate(), 'pagamento de boleto', 100, 0, 100, NEWID());
 
 
 select * from TB_MOVIMENTACAO
 
+
+INSERT INTO TB_CARTAO VALUES 
+	(1, 8392746150283746, 887, '2026-12-05', 1)
+
+
+GO
+
+SELECT * FROM TB_CARTAO
+
+INSERT INTO TB_CHAVE_PIX VALUES
+	(1, 'cpf', '69228182024', 1)
+
+
+GO
+
+
+
 COMMIT;
-ROLLBACK;
+--ROLLBACK;
